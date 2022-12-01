b9a052a844213982bb2dc5de39334f2f4e1189bf
joneikholmkea <jone@kea.dk>
2022-12-01 22:16:44 +0100

    Initial Commit

---
+++ SUIfirst/SUIfirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIfirstApp.swift
+//  SUIfirst
+//
+//  Created by Jon Eikholm on 01/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct SUIfirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
