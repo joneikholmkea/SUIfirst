0abe5088552b5fd0cd6a9354532dcf7e47a93b2e
joneikholmkea <jone@kea.dk>
2023-01-25 17:29:51 +0100

    new View added

---
+++ SUIfirst/DeleteMeView.swift
@@ -0,0 +1,22 @@
+//
+//  DeleteMeView.swift
+//  SUIfirst
+//
+//  Created by Jon Eikholm on 25/01/2023.
+//
+
+import SwiftUI
+
+struct DeleteMeView: View {
+    var body: some View {
+        Text("Hello, World! is a wonderful start")
+            .bold()
+            .font(Font.custom("Georgia", size:34))
+    }
+}
+
+struct DeleteMeView_Previews: PreviewProvider {
+    static var previews: some View {
+        DeleteMeView()
+    }
+}
